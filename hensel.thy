c291a253eb60fb9107db235c40b86965361e29e4
Andrew Duquette <duquea@mcmaster.ca>
2019-05-10 13:16:22 -0400

    Initial lemma attempts

--- hensel.thy
+++ hensel.thy
@@ -11,5 +11,27 @@ fun shift :: "(nat \<Rightarrow> 'a) \<Rightarrow> (nat \<Rightarrow> 'a)" where
 fun mult :: "('a, 'b) ring_scheme \<Rightarrow> (nat \<Rightarrow> 'a) \<Rightarrow> (nat \<Rightarrow> 'a)" where
   "mult R b n = add_pow R n (b n)"
 
-definition derivative :: "('a, 'b) ring_scheme \<Rightarrow> (nat \<Rightarrow> 'a ) \<Rightarrow> (nat \<Rightarrow> 'a)" where
-  "derivative R b n = shift (mult R b) n"
+definition deriv :: "('a, 'b) ring_scheme \<Rightarrow> (nat \<Rightarrow> 'a ) \<Rightarrow> (nat \<Rightarrow> 'a)" where
+  "deriv R b n = shift (mult R b) n"
+
+lemma mult_in_up_ring:
+  "mult R b \<in> up R" 
+proof
+  fix n
+  have "hensel.mult R b n \<in> carrier R" sledgehammer
+
+(* deriv also returns a polynomial *)
+lemma deriv_in_up_ring:
+  "(deriv R p) \<in> up R"
+proof-
+  assume "p \<in> up R"
+  then have "p n \<in> carrier R" 
+    by (simp add: mem_upD)
+  then have "shift p n \<in> carrier R"
+    by (simp add: \<open>p \<in> up R\<close> mem_upD)
+  have "mult R b n \<in> carrier R" using add_pow_def mult_def sledgehammer
+
+
+(*
+  obtain n where "deg R p = n" by simp
+  then have "n = (LEAST n. bound \<zero>\<^bsub>R\<^esub> n (coeff (UP R) p))" using deg_def  sledgehammer*)

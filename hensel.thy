ff2605820ea0a6d7345e321979b7863c63e4ce9e
Andrew Duquette <amranu@gmail.com>
2019-05-12 17:41:16 -0400

    Added some additional lemmas

--- hensel.thy
+++ hensel.thy
@@ -58,6 +58,51 @@ lemma(in domain) deriv_in_up_ring:
   shows "(deriv R p) \<in> up R" 
   by (simp add: assms deriv_def mult_in_up_ring shift_in_up_ring)
 
+lemma(in domain) gt_deg_is_zero:
+  assumes "p \<in> up R"
+  shows "\<And>m. \<lbrakk>m > deg R p\<rbrakk> \<Longrightarrow> p m = \<zero>"
+proof-
+  fix k
+  obtain n where 1: "deg R p = n" by simp
+  have 2: "deg R p = (LEAST n. bound (zero R) n (coeff (UP R) p))" using deg_def by auto
+  then have 3: "\<lbrakk> k > n \<rbrakk> \<Longrightarrow> k > (LEAST n. bound (zero R) n (coeff (UP R) p))"
+    using "1" by linarith
+  then have "\<lbrakk>k > deg R p\<rbrakk> \<Longrightarrow> p k = \<zero>"
+    by (smt LeastI UP_def \<open>deg R p = (LEAST n. bound \<zero> n (coeff (UP R) p))\<close> assms(1) bound_def bound_upD deg_def mem_Collect_eq restrict_apply up_def up_ring.simps(2))
+  then show "\<And>m. \<lbrakk>m > deg R p\<rbrakk> \<Longrightarrow> p m = \<zero>" using 1 2 3 assms(1) bound_def bound_upD deg_def mem_Collect_eq up_def up_ring.simps(2) 
+    by (smt UP_def cring_def domain_axioms domain_def restrict_apply ring.bound_upD wellorder_Least_lemma(1))
+qed
+
+lemma(in domain) bound_is_zero:
+  assumes "p \<in> up R"
+  assumes "deg R p = n"
+  shows "p n = \<zero>"
+proof-
+  obtain n where "n = (LEAST n. bound \<zero> n (coeff (UP R) p))"
+    by simp
+  have "n \<le> n+1" by simp
+  have "\<lbrakk>deg R p = n
+  then have "coeff (UP R) p (n+1) = \<zero>"
+
+
+
+
+lemma(in domain) deg_shift_lt:
+  assumes "p \<in> up R"
+  shows "deg R (shift p) = deg R p - 1" 
+proof-
+  have "\<exists>n. deg R p = n" by simp
+  obtain n where 1: "deg R p = n"
+    by auto
+  have "shift p n = p (n+1)"
+    by simp
+  have "p n = \<zero>" using 1 assms deg_def  sledgehammer
+
+lemma(in domain) deg_deriv_lt:
+  assumes "p \<in> up R"
+  assumes "deg R p = n"
+  shows "deg R (deriv R p) = n - 1" 
+
 
 (*
   obtain n where "deg R p = n" by simp
